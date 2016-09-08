b934ec44301b03bddd2e600a8c0516f45a3d5523
Anderson Hung <anderson.hung@live.com>
2016-09-08 16:03:46 +0000

    Refactored to use forEach

--- callbackIntro.js
+++ callbackIntro.js
@@ -1,15 +1,27 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
+
+
+/*  for (var i = 0; i < arr.length; i++) {
     if (arr[i] == "Waldo") {
       found(arr);   // execute callback
     }
+  }*/
+
+  function isWaldo(val, index){
+
+    if(val == "Waldo"){
+      found(index);
+    }
+
   }
+
+  arr.forEach(isWaldo);
+
 }
 
-function actionWhenFound(arr) {
-  var waldoIndex = arr.indexOf("Waldo");
-  console.log(waldoIndex);
+function actionWhenFound(val) {
+  console.log("Waldo is at position " + val + " in the array");
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
